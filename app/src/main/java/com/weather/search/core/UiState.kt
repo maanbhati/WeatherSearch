5751cd9e3b5eda123d3c7f9645ef08135f60af26
Mansingh Bhati <maanbhati@gmail.com>
2023-10-22 15:34:50 +0100

    Removed unused code.

--- app/src/main/java/com/weather/search/core/UiState.kt
+++ app/src/main/java/com/weather/search/core/UiState.kt
@@ -1,6 +1,6 @@
 package com.weather.search.core
 
-sealed class UiState<out T>() {
+sealed class UiState<out T> {
     data class Success<T>(val data: T) : UiState<T>()
     data class Error(val message: String) : UiState<Nothing>()
     object Loading : UiState<Nothing>()
